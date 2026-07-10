1b9d4dc48d57db5e3b8bac89cd1a2af24fbac757
Maxty40 <dhiaudin275@gmail.com>
2026-07-10 22:32:19 +0700

    Day 1 - Learning Variables and  Data Types

---
+++ main.js
@@ -0,0 +1,20 @@
+// Console log in JavaScript
+
+console.log("Hello World!"); // Print Hello World in Console
+
+// Variable in JavaScript
+
+var name = "Dhia"; // Var is used to declare a variable, also string is defined in double quotes ("")
+
+let age = 20; // Let is used to declare a block-scoped variable, also integer is defined without quotes
+
+const country = "Indonesia"; // Const is used to declare a constant variable
+
+let height = 167.5; // Float is defined without quotes and uses a decimal point (.)
+
+let isStudent = true; // Boolean is defined without quotes and can be either true or false
+
+let girlFriend = null; // Null is defined without quotes and represents the absence of any value
+
+// Alert in JavaScript
+alert(`Hello, my name is ${name}, I am ${age} years old. I live in ${country}. My height is ${height}. That is ${isStudent} I am a student, and I am have ${girlFriend} girlfrined :)`); // Show an alert box
