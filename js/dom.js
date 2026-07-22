f76fb67d253b8c553f8929683c0a70f1bdf9b6dd
Maxty40 <dhiaudin275@gmail.com>
2026-07-22 20:55:37 +0700

    Day 3 - DOM Manipulation

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+// Data selector
+const btn1 = document.getElementById("btn"); // Select button using ID
+const btn2 = document.querySelector(".selector"); // Select button using class
+
+// Styling from JS
+btn1.style.background = "lime";
+btn1.style.padding = "1rem";
+btn2.style.background = "orange";
