276d205ab0c99d13f425a538a2e0cde50a53d101
Maxty40 <dhiaudin275@gmail.com>
2026-07-22 21:19:34 +0700

    Day 3 - DOM

--- js/dom.js
+++ js/dom.js
@@ -6,3 +6,17 @@ const btn2 = document.querySelector(".selector"); // Select button using class
 btn1.style.background = "lime";
 btn1.style.padding = "1rem";
 btn2.style.background = "orange";
+
+// Event Listener
+function changeColor() { // Changing button color when clicked
+    btn1.style.background = "aqua";
+    console.log("Hi")
+}
+
+function changeText() {
+    btn2.textContent = "Hi"
+}
+
+function resetText() {
+    btn2.textContent = "Click Me"
+}
