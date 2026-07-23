8ded2c301cb8dc5711c56a0eba230733ec2d0947
Maxty40 <dhiaudin275@gmail.com>
2026-07-23 20:47:06 +0700

    Day 4 - Advanced Array

---
+++ js/array2.js
@@ -0,0 +1,26 @@
+//  Make an array
+const myArray = ['1', '2', '3']
+console.log(myArray)
+
+// Create empty array
+const yourArray = []
+console.log(yourArray)
+
+// Adding data to array using index
+yourArray[0] = 'Hi'
+yourArray[1] = 'Hello'
+
+console.log(yourArray)
+
+// Another way to create an array
+const theirArray = new Array()
+theirArray[0]= 'Jakarta'
+
+// Adding data by skipping indexes
+theirArray[3] = 'Bogor'
+
+// Picking data from array by index
+console.log(theirArray[3])
+
+// Picking an undefined array data
+console.log(theirArray[1])
